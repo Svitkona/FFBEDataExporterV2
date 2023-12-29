c7aa2fdef7df9e3e1c385c15c847e80844fc670b
Svitkona <svitkona5@gmail.com>
2023-12-29 14:33:42 +0400

    Add build script to generate zip file

---
+++ build.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const archiver = require('archiver');
+
+async function main() {
+  try {
+    await fsPromises.mkdir('out');
+  } catch (error) {
+    if (error.code !== 'EEXIST') {
+      throw error;
+    }
+  }
+
+  const output = fs.createWriteStream(`${__dirname}/out/ffbesyncv2.zip`);
+
+  const archive = archiver('zip');
+
+  archive.directory(`${__dirname}/build/`, false);
+
+  archive.pipe(output);
+}
+
+main();
