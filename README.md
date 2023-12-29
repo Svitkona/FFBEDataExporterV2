41e300da5950ccca6e5cb6bc0a405d15ac6b15e7
Svitkona <svitkona5@gmail.com>
2023-12-29 13:42:20 +0400

    Use normal page instead of popup

--- README.md
+++ README.md
@@ -4,7 +4,34 @@ This Chrome/Firefox extension is for exporting your data from FFBE.
 
 ### Building
 
-Install NodeJS. Run `npm install` and `npm build`. The build will create a new directory called `build` which will contain all the files needed to install the extension
+1. First, decide whether you are targetting Chrome or Firefox. For Chrome, skip to step 2. For Firefox, the `manifest.json` file needs to be modified: replace
+
+   ```
+   "background": {
+     "service_worker": "./static/js/background.js"
+   },
+   ```
+
+   with
+
+   ```
+   "background": {
+     "scripts": ["./static/js/background.js"]
+   },
+   ```
+
+2. Install NodeJS
+3. Run `npm install` and `npm build`. The build will create a new directory called `build` which will contain all the files needed to install the extension
+
+### Creating an OAuth credential for Google
+
+- Navigate to `https://console.cloud.google.com/apis/`
+- Create a new project if needed
+- Click on `Credentials` on the left side of the screen
+- Click `CREATE CREDENTIALS` and select `OAuth client ID`
+- Select `Web application`
+- Click `Create`
+- Copy the Client ID and replace `client_id` in `src/background/index.ts` with the copied value
 
 ### Installing the extension
 
@@ -17,6 +44,8 @@ Chrome:
 Firefox:
 
 - Pack the contents of the `build` directory into a .zip file
+- Sign the extension
+  - This is a fairly complicated step that is beyond the scope of this README. See https://extensionworkshop.com/documentation/publish/ for more information
 - Go to `about:addons`
 - Click `Extensions` on the left side of the screen
 - Click on the settings icon on the same line as the `Manage Your Extensions` header
--- src/background/index.ts
+++ src/background/index.ts
@@ -1,10 +1,21 @@
+import CryptoJS from 'crypto-js';
 import Browser, { runtime } from 'webextension-polyfill';
+import ky from 'ky';
 
 runtime.onInstalled.addListener(() => {
   console.log('[background] loaded');
+  console.log(
+    `[background] redirect URI: ${Browser.identity.getRedirectURL('oauth2')}`,
+  );
 });
 
-runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+Browser.action.onClicked.addListener(() => {
+  Browser.tabs.create({
+    url: runtime.getURL('index.html'),
+  });
+});
+
+runtime.onMessage.addListener(async message => {
   if (message.type === 'googleAuth') {
     const authParams = {
       client_id:
@@ -35,7 +46,256 @@ runtime.onMessage.addListener(async (message, sender, sendResponse) => {
         data: error,
       };
     }
+  } else if (message.type === 'startFfbeExport') {
+    if (!message.data) {
+      return {
+        type: 'error',
+        data: {
+          message: "Missing params for 'startFfbeExport'",
+        },
+      };
+    }
+
+    const { accountId, oauthToken, isGoogle } = message.data;
+    if (!accountId || !oauthToken || !isGoogle) {
+      return {
+        type: 'error',
+        data: {
+          message: "Missing params for 'startFfbeExport'",
+        },
+      };
+    }
+
+    const res = await initateDataExport(accountId, oauthToken, isGoogle);
+
+    return res;
   }
 });
 
+function makeAuthenticationPayload(
+  accountId: string,
+  authToken: string,
+  isGoogle: boolean,
+  actionKey: string,
+) {
+  const payload = isGoogle
+    ? '{"LhVz6aD2":[{"6Nf5risL":"0","40w6brpQ":"0","jHstiL12":"0","io30YcLA":"Nexus 6P_android6.0","K1G4fBjF":"2","e8Si6TGh":"","1WKh6Xqe":"ver.2.7.0.1","64anJRhx":"2019-02-08 11:15:15","Y76dKryw":null,"6e4ik6kA":"","NggnPgQC":"","e5zgvyv7":"' +
+      authToken +
+      '","GwtMEDfU":"' +
+      accountId +
+      '"}],"Euv8cncS":[{"K2jzG6bp":"1"}],"c402FmRD":[{"kZdGGshD":"2"}],"c1qYg84Q":[{"a4hXTIm0":"F_APP_VERSION_AND","wM9AfX6I":"10000"},{"a4hXTIm0":"F_RSC_VERSION","wM9AfX6I":"0"},{"a4hXTIm0":"F_MST_VERSION","wM9AfX6I":"2047"}]}'
+    : '{"LhVz6aD2":[{"9Tbns0eI":null,"9qh17ZUf":null,"6Nf5risL":"0","io30YcLA":"Nexus 6P_android6.0","K1G4fBjF":"2","e8Si6TGh":"","U7CPaH9B":null,"1WKh6Xqe":"ver.2.7.0.1","64anJRhx":"2019-02-08 11:15:15","Y76dKryw":null,"6e4ik6kA":"","NggnPgQC":"","X6jT6zrQ":null,"DOFV3qRF":null,"P_FB_TOKEN":"' +
+      authToken +
+      '","P_FB_ID":"' +
+      accountId +
+      '"}],"Euv8cncS":[{"K2jzG6bp":"0"}],"c1qYg84Q":[{"a4hXTIm0":"F_APP_VERSION_IOS","wM9AfX6I":"10000"},{"a4hXTIm0":"F_RSC_VERSION","wM9AfX6I":"0"},{"a4hXTIm0":"F_MST_VERSION","wM9AfX6I":"377"}]}';
+
+  const encrypted = CryptoJS.AES.encrypt(
+    payload,
+    CryptoJS.enc.Utf8.parse(actionKey),
+    {
+      mode: CryptoJS.mode.ECB,
+      padding: CryptoJS.pad.Pkcs7,
+    },
+  );
+
+  return {
+    payload:
+      '{"TEAYk6R1":{"ytHoz4E2":"75527","z5hB3P01":"75fYdNxq"},"t7n6cVWf":{"qrVcDe48":"' +
+      encrypted.ciphertext.toString(CryptoJS.enc.Base64) +
+      '"}}',
+  };
+}
+
+async function callActionSymbol(
+  actionSymbol: string,
+  actionKey: string,
+  data: { payload: string },
+) {
+  const res = await ky
+    .post(
+      `https://lapis340v-gndgr.gumi.sg/lapisProd/app/php/gme/actionSymbol/${actionSymbol}.php`,
+      {
+        body: data.payload,
+      },
+    )
+    .json();
+
+  // TODO: check response and fix any
+  const encryptedPayloadBase64 = (res as any)['t7n6cVWf']['qrVcDe48'];
+
+  const decrypted = CryptoJS.AES.decrypt(
+    encryptedPayloadBase64.toString(),
+    CryptoJS.enc.Utf8.parse(actionKey),
+    { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 },
+  );
+
+  const obj = decrypted.toString(CryptoJS.enc.Utf8);
+
+  return JSON.parse(obj);
+}
+
+function makeLoginToken(
+  accountId: string,
+  authToken: string,
+  isGoogle: boolean,
+  data: any, // TODO: fix
+) {
+  if (isGoogle) {
+    return JSON.stringify({
+      LhVz6aD2: [
+        {
+          '9qh17ZUf': data['LhVz6aD2'][0]['9qh17ZUf'],
+          JC61TPqS: data['LhVz6aD2'][0]['JC61TPqS'],
+          '6Nf5risL': data['LhVz6aD2'][0]['6Nf5risL'],
+          '40w6brpQ': '0',
+          jHstiL12: '0',
+          io30YcLA: 'Nexus 6P_android6.0',
+          K1G4fBjF: '2',
+          e8Si6TGh: data['LhVz6aD2'][0]['e8Si6TGh'],
+          '1WKh6Xqe': 'ver.2.7.0.1',
+          '64anJRhx': '2019-02-08 11:15:15',
+          m3Wghr1j: data['LhVz6aD2'][0]['m3Wghr1j'],
+          ma6Ac53v: '0',
+          D2I1Vtog: '0',
+          '9K0Pzcpd': '10000',
+          mESKDlqL: data['LhVz6aD2'][0]['mESKDlqL'],
+          iVN1HD3p: data['LhVz6aD2'][0]['iVN1HD3p'],
+          Y76dKryw: null,
+          '6e4ik6kA': '',
+          NggnPgQC: data['LhVz6aD2'][0]['NggnPgQC'],
+          GwtMEDfU: accountId,
+          e5zgvyv7: authToken,
+          '9Tbns0eI': data['LhVz6aD2'][0]['9Tbns0eI'],
+        },
+      ],
+      QCcFB3h9: [
+        {
+          qrVcDe48: data['QCcFB3h9'][0]['qrVcDe48'],
+        },
+      ],
+      c1qYg84Q: [
+        {
+          a4hXTIm0: 'F_APP_VERSION_AND',
+          wM9AfX6I: '10000',
+        },
+        {
+          a4hXTIm0: 'F_RSC_VERSION',
+          wM9AfX6I: '0',
+        },
+        {
+          a4hXTIm0: 'F_MST_VERSION',
+          wM9AfX6I: '10000',
+        },
+      ],
+    });
+  } else {
+    throw new Error('Unimplemented.');
+  }
+}
+
+function makeUserInfoPayload(
+  actionKey: string,
+  payloadKey: string,
+  loginToken: string,
+) {
+  const secondEncrypted = CryptoJS.AES.encrypt(
+    loginToken,
+    CryptoJS.enc.Utf8.parse(actionKey),
+    { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 },
+  );
+
+  const payload = `{"TEAYk6R1":{"ytHoz4E2":"75528","z5hB3P01":"${payloadKey}"},"t7n6cVWf":{"qrVcDe48":"${secondEncrypted.ciphertext.toString(
+    CryptoJS.enc.Base64,
+  )}"}}`;
+
+  return payload;
+}
+
+async function initateDataExport(
+  accountId: string,
+  oauthToken: string,
+  isGoogle: boolean,
+) {
+  const loginUrlSymbol = 'fSG1eXI9';
+  const loginKey = 'rVG09Xnt\0\0\0\0\0\0\0\0';
+
+  const userInfo1UrlSymbol = 'u7sHDCg4';
+  const userInfo1Key = 'rcsq2eG7\0\0\0\0\0\0\0\0';
+  const userInfo1PayloadKey = 'X07iYtp5';
+
+  const userInfo2UrlSymbol = '7KZ4Wvuw';
+  const userInfo2Key = '7VNRi6Dk\0\0\0\0\0\0\0\0';
+  const userInfo2PayloadKey = '2eK5Vkr8';
+
+  const userInfo3UrlSymbol = 'lZXr14iy';
+  const userInfo3Key = '0Dn4hbWC\0\0\0\0\0\0\0\0';
+  const userInfo3PayloadKey = '4rjw5pnv';
+
+  try {
+    const initialAuthenticationPayload = makeAuthenticationPayload(
+      accountId,
+      oauthToken,
+      isGoogle,
+      loginKey,
+    );
+
+    const initialAuthenticationRes = await callActionSymbol(
+      loginUrlSymbol,
+      loginKey,
+      initialAuthenticationPayload,
+    );
+
+    const loginToken = makeLoginToken(
+      accountId,
+      oauthToken,
+      isGoogle,
+      initialAuthenticationRes,
+    );
+
+    const userData1Payload = makeUserInfoPayload(
+      userInfo1Key,
+      userInfo1PayloadKey,
+      loginToken,
+    );
+
+    const userData1 = await callActionSymbol(userInfo1UrlSymbol, userInfo1Key, {
+      payload: userData1Payload,
+    });
+
+    const userData2Payload = makeUserInfoPayload(
+      userInfo2Key,
+      userInfo2PayloadKey,
+      loginToken,
+    );
+
+    const userData2 = await callActionSymbol(userInfo2UrlSymbol, userInfo2Key, {
+      payload: userData2Payload,
+    });
+
+    const userData3Payload = makeUserInfoPayload(
+      userInfo3Key,
+      userInfo3PayloadKey,
+      loginToken,
+    );
+
+    const userData3 = await callActionSymbol(userInfo3UrlSymbol, userInfo3Key, {
+      payload: userData3Payload,
+    });
+
+    return {
+      type: 'success',
+      data: {
+        userData1,
+        userData2,
+        userData3,
+      },
+    };
+  } catch (error) {
+    return {
+      type: 'error',
+      data: (error as Error).message,
+    };
+  }
+}
 export {};
